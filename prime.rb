90a62ca670910a1bdd24235b9c8c41d2c79c9482
Christopher Dutcher <cadutcher@icloud.com>
2020-06-23 21:25:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(integer)
+  integers = (-10..110000)
+  integers_array = (-10..110000).to_a
+  x = 0
+  while x < integers_array.length
+    integers_array.delete if {|x| x == x || x == 1}
+    integers_array[x].any?
+
+end
