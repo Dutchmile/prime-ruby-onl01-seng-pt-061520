b157db16583acc215ff4e0274568baebde2a8148
Christopher Dutcher <cadutcher@icloud.com>
2020-06-23 21:43:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+  x = 2
+  if integer < 2
+    return false
+  else
+  while x < integer
+    return false if integer % x == 0
+    x += 1
+  end
+end
+  true
+end
+
+prime?(2)
